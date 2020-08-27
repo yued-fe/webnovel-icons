e72e08d5cd600c5ff28c4a98fdd5d3ac7bbab679
27 <zhengchengzhong@yuewen.com>
2020-08-27 17:45:37 +0800

    feat: update

--- bin/template.js
+++ bin/template.js
@@ -1,12 +1,12 @@
 const getElementCode = (ComponentName, svgCode) => `
-  import * as React from "react";
-  function ${ComponentName}(props){
-    return (
-      <svg width="24" height="24" fill="none" {...props}>
-        ${svgCode}
-      </svg>
-    );
-  }
-  export default ${ComponentName};
+import * as React from "react";
+function ${ComponentName}(props){
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+      ${svgCode}
+    </svg>
+  );
+}
+export default ${ComponentName};
 `;
 module.exports = getElementCode;
