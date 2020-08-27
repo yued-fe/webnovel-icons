5ebc509d1f4c478aad67ec7dba03d985a0b6720e
27 <zhengchengzhong@yuewen.com>
2020-08-27 16:38:42 +0800

    feat: 更新逻辑

--- bin/build.js
+++ bin/build.js
@@ -1,122 +1,86 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable prefer-template */
-const path = require("path");
-const fs = require("fs");
-const format = require("prettier-eslint");
-const processSvg = require("./processSvg");
-const { parseName } = require("./utils");
-const defaultStyle = process.env.npm_package_config_style || "stroke";
-const { getAttrs, getElementCode } = require("./template");
-const icons = require("../src/data.json");
+const path = require('path');
+const fs = require('fs');
+const upperCamelCase = require('uppercamelcase');
+const processSvg = require('./processSvg');
+const getElementCode = require('./template');
+const icons = require('../src/data.json');
 
-const rootDir = path.join(__dirname, "..");
+// rootDir
+const rootDir = path.join(__dirname, '..');
 
 // where icons code in
-const srcDir = path.join(rootDir, "dist");
-const iconsDir = path.join(rootDir, "dist/icons");
+const srcDir = path.join(rootDir, 'dist');
+const iconsDir = path.join(rootDir, 'dist/icons');
 
 // where index.js code in
-const iconFile = path.join(rootDir, "dist", "index.js");
-const iconFileD = path.join(rootDir, "dist", "index.d.ts");
+const iconFile = path.join(rootDir, 'dist', 'index.js');
+const iconFileD = path.join(rootDir, 'dist', 'index.d.ts');
 
-// generate icons.js and icons.d.ts file
-const generateIconsIndex = () => {
-  if (!fs.existsSync(srcDir)) {
-    fs.mkdirSync(srcDir);
-    fs.mkdirSync(iconsDir);
-  } else if (!fs.existsSync(iconsDir)) {
-    fs.mkdirSync(iconsDir);
-  }
+const _api = {
+  // generate icon code separately
+  generateIconCode: async ({ name }) => {
+    const ComponentName = `I${upperCamelCase(name)}`;
+    // console.log(names);
+    const location = path.join(rootDir, 'src/svg', `${name}.svg`); 
+    const code = fs.readFileSync(location);
+    const svgCode = await processSvg(code);
+    const element = getElementCode(ComponentName, svgCode);
+    const destination = path.join(rootDir, 'dist/icons', `${ComponentName}.js`);
+    fs.writeFileSync(destination, element, 'utf-8');
+    console.log('Successfully built', ComponentName);
+    return { ComponentName, name: ComponentName };
+  },
+  // generate icons.js and icons.d.ts file
+  generateIconsIndex: () => {
+    if (!fs.existsSync(srcDir)) {
+      fs.mkdirSync(srcDir);
+      fs.mkdirSync(iconsDir);
+    } else if (!fs.existsSync(iconsDir)) {
+      fs.mkdirSync(iconsDir);
+    }
+    const initialTypeDefinitions = `
+import { SVGAttributes } from 'react';
+type Icon = (props?: SVGAttributes<SVGElement>) => JSX.Element;
 
-  const initialTypeDefinitions = `/// <reference types="react" />
-  import { ComponentType, SVGAttributes } from 'react';
-
-  interface Props extends SVGAttributes<SVGElement> {
-    size?: string | number;
-  }
-
-  type Icon = ComponentType<Props>;
-  `;
-
-  fs.writeFileSync(iconFile, "", "utf-8");
-  fs.writeFileSync(iconFileD, initialTypeDefinitions, "utf-8");
-};
-
-// generate attributes code
-const attrsToString = (attrs, style) => {
-  // console.log("style: ", style);
-  return Object.keys(attrs)
-    .map((key) => {
-      // should distinguish fill or stroke
-      if(key==="fill" || key==="stroke"){
-        return key + '="' + attrs[key] + '"';
+// export icons
+`;
+    fs.writeFileSync(iconFile, '', 'utf-8');
+    fs.writeFileSync(iconFileD, initialTypeDefinitions, 'utf-8');
+  },
+  // append export code to icons.js
+  appendToIconsIndex: infos => {
+    let exportString = '';
+    let exportTypeString = '';
+    infos.map(({ ComponentName, name }) => {
+      exportString += `export { default as ${ComponentName} } from './icons/${name}';\r\n`;
+      exportTypeString += `export const ${ComponentName}: Icon;\n`;
+    });
+    fs.writeFileSync(iconFile, exportString, 'utf-8');
+    fs.appendFileSync(iconFileD, exportTypeString, 'utf-8');
+  },
+  getComponentsInfo: async function() {
+    const infoComponents = [];
+    const iconKeys = Object.keys(icons).map(key => icons[key]);
+    for (const { name } of iconKeys) {
+      // ignore the icon with '_' start
+      const isIgnore = name.charAt(0) === '_';
+      if (!isIgnore) {
+        await _api
+          .generateIconCode({ name })
+          .then(({ ComponentName, name }) => {
+            infoComponents.push({ ComponentName, name });
+          });
       }
-      if (key === "width" || key === "height" || key === style) {
-        return key + "={" + attrs[key] + "}";
-      }
-      if (key === "otherProps") {
-        return "{...otherProps}";
-      }
-      return key + '="' + attrs[key] + '"';
-    })
-    .join(" ");
+    }
+    return infoComponents;
+  },
+  init: function() {
+    // create base file icons.js and icons.d.ts
+    _api.generateIconsIndex();
+    _api.getComponentsInfo().then(_api.appendToIconsIndex);
+  },
 };
 
-// generate icon code separately
-const generateIconCode = async ({ name }) => {
-  const names = parseName(name, defaultStyle);
-  // console.log(names);
-  const location = path.join(rootDir, "src/svg", `${names.name}.svg`);
-  const destination = path.join(rootDir, "dist/icons", `${names.name}.js`);
-  const code = fs.readFileSync(location);
-  const svgCode = await processSvg(code);
-  const ComponentName = `I${names.componentName}`;
-  const element = getElementCode(
-    ComponentName,
-    attrsToString(getAttrs(names.style), names.style),
-    svgCode
-  );
-  const component = format({
-    text: element,
-    eslintConfig: {
-      extends: "airbnb",
-    },
-    prettierOptions: {
-      bracketSpacing: true,
-      singleQuote: true,
-      parser: "flow",
-    },
-  });
-  fs.writeFileSync(destination, component, "utf-8");
-  console.log("Successfully built", ComponentName);
-  return { ComponentName, name: names.name };
-};
-
-// append export code to icons.js
-const appendToIconsIndex = (infos) => {
-  let exportString = "";
-  let exportTypeString = "";
-  infos.map(({ ComponentName, name }) => {
-    exportString += `export { default as ${ComponentName} } from './icons/${name}';\r\n`;
-    exportTypeString += `export const ${ComponentName}: Icon;\n`;
-  });
-  fs.writeFileSync(iconFile, exportString, "utf-8");
-  fs.appendFileSync(iconFileD, exportTypeString, "utf-8");
-};
-
-generateIconsIndex();
-
-const getComponentsInfo = async function() {
-  const infoComponents = [];
-  const iconKeys = Object.keys(icons).map((key) => icons[key]);
-  for (const { name } of iconKeys) {
-    // ignore the icon with '_' start
-    const isIgnore = name.charAt(0) === "_";
-    !isIgnore &&
-      (await generateIconCode({ name }).then(({ ComponentName, name }) => {
-        infoComponents.push({ ComponentName, name });
-      }));
-  }
-  return infoComponents;
-};
-getComponentsInfo().then(appendToIconsIndex);
+_api.init();
--- bin/template.js
+++ bin/template.js
@@ -1,33 +1,12 @@
-const getAttrs = (style) => {
-  const baseAttrs = {
-    xmlns: 'http://www.w3.org/2000/svg',
-    width: 'size',
-    height: 'size',
-    viewBox: '0 0 24 24',
-  }
-  const fillAttrs = {
-    fill: 'currentColor',
-    otherProps: '...otherProps'
-  }
-  const strokeAttrs = {
-    fill: 'none',
-    stroke: 'currentColor',
-    strokeWidth: 2,
-    strokeLinecap: 'round',
-    strokeLinejoin: 'round',
-    otherProps: '...otherProps'
-  }
-  return Object.assign({}, baseAttrs, style==='fill' ? fillAttrs : strokeAttrs)
-}
-
-const getElementCode = (ComponentName, attrs, svgCode) => `
-  import React from 'react';
-  const ${ComponentName} = ({ size="24", ...otherProps }) => (
-    <svg ${attrs}>
+const getElementCode = (ComponentName, svgCode) => `
+  import * as React from "react";
+  function ${ComponentName}(props){
+    return (
+      <svg width="24" height="24" fill="none" {...props}>
         ${svgCode}
-    </svg>
-  );
-  export default ${ComponentName}
+      </svg>
+    );
+  }
+  export default ${ComponentName};
 `;
-
-module.exports = { getAttrs, getElementCode }
+module.exports = getElementCode;
