60348896e47568d85c6cb416a74cca3c9debd64c
27 <zhengchengzhong@yuewen.com>
2020-09-02 11:48:04 +0800

    fix: 去重和排序

--- bin/build.js
+++ bin/build.js
@@ -20,17 +20,17 @@ const iconFileD = path.join(rootDir, 'dist', 'index.d.ts');
 
 const _api = {
   // generate icon code separately
-  generateIconCode: async ({ name }) => {
+  generateIconCode: async ({name}) => {
     const ComponentName = `I${upperCamelCase(name)}`;
     // console.log(names);
-    const location = path.join(rootDir, 'src/svg', `${name}.svg`); 
+    const location = path.join(rootDir, 'src/svg', `${name}.svg`);
     const code = fs.readFileSync(location);
     const svgCode = await processSvg(code);
     const element = getElementCode(ComponentName, svgCode);
     const destination = path.join(rootDir, 'dist/icons', `${ComponentName}.js`);
     fs.writeFileSync(destination, element, 'utf-8');
     console.log('Successfully built', ComponentName);
-    return { ComponentName, name: ComponentName };
+    return ComponentName;
   },
   // generate icons.js and icons.d.ts file
   generateIconsIndex: () => {
@@ -50,33 +50,33 @@ type Icon = (props?: SVGAttributes<SVGElement>) => JSX.Element;
     fs.writeFileSync(iconFileD, initialTypeDefinitions, 'utf-8');
   },
   // append export code to icons.js
-  appendToIconsIndex: infos => {
+  appendToIconsIndex: (ComponentNames = []) => {
     let exportString = '';
     let exportTypeString = '';
-    infos.map(({ ComponentName, name }) => {
-      exportString += `export { default as ${ComponentName} } from './icons/${name}';\r\n`;
+    [...new Set(ComponentNames)].sort().map((ComponentName)=>{
+      exportString += `export { default as ${ComponentName} } from './icons/${ComponentName}';\r\n`;
       exportTypeString += `export const ${ComponentName}: Icon;\n`;
     });
     fs.writeFileSync(iconFile, exportString, 'utf-8');
     fs.appendFileSync(iconFileD, exportTypeString, 'utf-8');
   },
-  getComponentsInfo: async function() {
+  getComponentsInfo: async function () {
     const infoComponents = [];
     const iconKeys = Object.keys(icons).map(key => icons[key]);
-    for (const { name } of iconKeys) {
+    for (const {name} of iconKeys) {
       // ignore the icon with '_' start
       const isIgnore = name.charAt(0) === '_';
       if (!isIgnore) {
         await _api
-          .generateIconCode({ name })
-          .then(({ ComponentName, name }) => {
-            infoComponents.push({ ComponentName, name });
+          .generateIconCode({name})
+          .then(ComponentName => {
+            infoComponents.push(ComponentName);
           });
       }
     }
     return infoComponents;
   },
-  init: function() {
+  init: function () {
     // create base file icons.js and icons.d.ts
     _api.generateIconsIndex();
     _api.getComponentsInfo().then(_api.appendToIconsIndex);
