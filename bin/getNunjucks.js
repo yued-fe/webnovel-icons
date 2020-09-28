11a9cbafe4aa318b9d0af80a376e55f568221f2e
27 <zhengchengzhong@yuewen.com>
2020-09-28 14:27:11 +0800

    fix: 修改自定义大小

--- bin/getNunjucks.js
+++ bin/getNunjucks.js
@@ -1,5 +1,5 @@
 const getNunjucks = (ComponentName, svgCode) => `
-{% macro ${ComponentName}(size='16', class, attr='') %}
+{% macro ${ComponentName}(size='16', class='', attr='') %}
 <svg width="{{ size | safe }}" height="{{ size | safe  }}" viewBox="0 0 24 24" {% if class %}class="{{ class | safe }}"{% endif %} {{ attr | safe }}>
     ${svgCode}
 </svg>
