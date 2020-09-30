5150bae3f17929a7674322225f31a6788d444d9a
27 <zhengchengzhong@yuewen.com>
2020-09-30 13:33:14 +0800

    fix: 格式化symbol

--- bin/getNjsSymbol.js
+++ bin/getNjsSymbol.js
@@ -1,5 +1,6 @@
 const getNjsSymbol = (ComponentName, svgCode, attrs = {}) => {
   const {viewBox = "0 0 24 24"} = attrs;
-  return `{% macro ${ComponentName}() %}<symbol id="${ComponentName}" viewBox="${viewBox}">${svgCode}</symbol>{% endmacro %}`;
+  return `{% macro ${ComponentName}() %}<symbol id="${ComponentName}" viewBox="${viewBox}">${svgCode}</symbol>{% endmacro %}
+`;
 };
 module.exports = getNjsSymbol;
